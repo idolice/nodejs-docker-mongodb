e19409f5348033096c2aa6f372756738b5231031
nlin <nlin@thoughtworks.com>
2017-02-17 17:18:09 +0800

    add cors access

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.configure(function () {
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 });
 
 app.configure('development', function () {
